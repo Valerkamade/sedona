25ca31a12114984cdba4c6bbe04025250cdcb80b
Valeriya Izotova <my@valerka-made.ru>
2023-01-19 09:50:12 +0300

    fix & style: поправить некоторые стили и добавть картинки

--- source/js/menu.js
+++ source/js/menu.js
@@ -6,3 +6,30 @@ const openClose = function () {
 };
 
 toggle.addEventListener("click", openClose);
+
+initialCards.forEach(function (item) {
+  const galleryElement = galleryTemplate.querySelector('.elements__element').cloneNode(true);
+  galleryElement.querySelector('.elements__title').textContent = item.name;
+  galleryElement.querySelector('.elements__img').src = item.link;
+  const buttonLike = galleryElement.querySelector('.elements__button-like');
+  buttonLike.addEventListener('click', function (event) {
+  const eventTarget = event.target;
+  eventTarget.classList.toggle('elements__button-like_active');
+  })
+  sectionElements.append(galleryElement);
+  });
+
+  // а так нет
+  initialCards.forEach(function (item) {
+  const galleryElement = galleryTemplate.querySelector('.elements__element').cloneNode(true);
+  galleryElement.querySelector('.elements__title').textContent = item.name;
+  galleryElement.querySelector('.elements__img').src = item.link;
+  sectionElements.append(galleryElement);
+  });
+
+  const galleryEl = galleryTemplate.querySelector('.elements__element')
+  const buttonLike = galleryEl.querySelector('.elements__button-like');
+  buttonLike.addEventListener('click', function (event) {
+  const eventTarget = event.target;
+  eventTarget.classList.toggle('elements__button-like_active');
+  })
